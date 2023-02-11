5071857abf9618cce1c290e6e51fff5ad6aa5169
Shiyaam <63956643+shiyaamsunder@users.noreply.github.com>
2023-02-11 22:10:17 +0530

    all programs finished

--- 07.12.2022/LAB4/1_greet.py
+++ 07.12.2022/LAB4/1_greet.py
@@ -1,5 +1,5 @@
 def greet(name):
     print("Hello", name)
 
-name = input("Enter your name: ")
+        name = input("Enter your name: ")
 greet(name)
---
+++ 18.01.2023/01/01.py
@@ -0,0 +1,6 @@
+"""
+1.	Write a program that reads text from a file and writes it into another file
+    but in the reverse order.
+"""
+f = open("file.txt", "r")
+print(f.read())
